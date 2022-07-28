38dfd9fb9fc88984afc04e9642e13b08d4199ea7
JinTae <kab7210@naver.com>
2022-07-28 13:55:23 +0900

    add ChakraProvider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Login from "../components/Login";
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Login />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
